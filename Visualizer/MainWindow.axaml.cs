5435f4573b57e0d93ef171bfdcb2677b5354361d
JakubMarecek <jakubmarecek715@gmail.com>
2024-09-27 22:49:50 +0200

    scnHubNode In ordinal dynamic adding

--- Visualizer/MainWindow.axaml.cs
+++ Visualizer/MainWindow.axaml.cs
@@ -182,7 +182,7 @@ namespace Visualizer
 									}
 
 									var outputName = "";
-									if (outScksMapp == null) outputName = Data.GetOutputsNames(nodeType, sck.SelectToken("stamp.name").ToObject<int>());
+									if (outScksMapp == null) outputName = GetOutputsNames(nodeType, sck.SelectToken("stamp.name").ToObject<int>());
 
 									itemOuts.Add(new()
 									{
@@ -230,13 +230,15 @@ namespace Visualizer
 							var rootChunk = jsonData.SelectToken("Data.RootChunk");
 
 							Dictionary<string, string> prms = [];
-							if (nodeType == "scnQuestNode") prms = NodeProperties.GetPropertiesForQuestNode(it.SelectToken("questNode.Data"), rootChunk);
-							else prms = NodeProperties.GetPropertiesForSectionNode(it, rootChunk);
+							if (nodeType == "scnQuestNode")
+								prms = NodeProperties.GetPropertiesForQuestNode(it.SelectToken("questNode.Data"), rootChunk);
+							else
+								prms = NodeProperties.GetPropertiesForSectionNode(it, rootChunk);
 							//if (nodeType == "scnSectionNode" || nodeType == "scnRewindableSectionNode") prms = NodeProperties.GetPropertiesForSectionNode(it);
 							//if (nodeType == "scnStartNode" || nodeType == "scnEndNode") prms = NodeProperties.GetPropertiesForSectionNode(it, rootChunk);
 
 							var ntbName = "";
-							if (NotablePoints.ContainsKey(id)) ntbName = " - " + NotablePoints[id];
+							if (NotablePoints.TryGetValue(id, out string value)) ntbName = " - " + value;
 
 							int highestName = 0;
 							int highestOrdinal = 0;
@@ -261,7 +263,7 @@ namespace Visualizer
 							{
 								var inputVarCount = it.SelectToken("numInSockets")?.Value<int>() ?? 1;
 
-								itemInps = Data.GetInputsNames(nodeType, inputVarCount);
+								itemInps = GetInputsNames(nodeType, inputVarCount);
 
 								foreach (var itemInp in itemInps)
 								{
@@ -695,8 +697,18 @@ namespace Visualizer
 								var p = Items[sub.DestinationID];
 								{
 									for (int j = 0; j < p.Inputs.Count; j++)
-									{
-										if ((!isQuest && sub.Name == p.Inputs[j].Name && sub.Ordinal == p.Inputs[j].Ordinal) || (isQuest && sub.HandleID == p.Inputs[j].HandleID))
+                                    {
+                                        if (!isQuest)
+                                        {
+                                            var ord = int.Parse(sub.Ordinal);
+                                            if (p.Name == "scnHubNode" && ord > p.HighestOrdinal)
+                                            {
+                                                p.Inputs.Add(new() { InputName = "In", Name = "0", Ordinal = sub.Ordinal });
+                                                p.HighestOrdinal++;
+                                            }
+                                        }
+
+                                        if ((!isQuest && sub.Name == p.Inputs[j].Name && sub.Ordinal == p.Inputs[j].Ordinal) || (isQuest && sub.HandleID == p.Inputs[j].HandleID))
 										{
 											p.Inputs[j].IsUsed = true;
 										}
@@ -854,8 +866,8 @@ namespace Visualizer
 								{
 									var p = Items[sub.DestinationID];
 									if (p.Draw)
-									{
-										for (int j = 0; j < p.Inputs.Count; j++)
+                                    {
+                                        for (int j = 0; j < p.Inputs.Count; j++)
 										{
 											if (
 												//(!isQuest && sub.Value.Ordinal == p.Inputs[j].Ins || (sub.Value.Name == "0" && p.Inputs[j].Ins == "gen_in") || (sub.Value.Name == "1" && p.Inputs[j].Ins == "gen_cut")) ||
@@ -971,7 +983,99 @@ namespace Visualizer
 			}
 		}
 
-		List<Color> linesColors = new();
+        public static string GetOutputsNames(string nodeType, int index)
+        {
+            var name = "";
+
+            if (nodeType == "scnChoiceNode")
+            {
+                if (index == 0) name = "Option";
+                if (index == 1) name = "AnyOption";
+                if (index == 2) name = "Immediate";
+                if (index == 3) name = "CancelFwd";
+                if (index == 4) name = "NoOption";
+                if (index == 5) name = "WhenDisplayed";
+                if (index == 6) name = "Reminder";
+            }
+            else if (nodeType == "scnSectionNode")
+            {
+                if (index == 0) name = "Out";
+                if (index == 1) name = "CancelForward";
+                //if (index == 2) name = "TransmitSignal";
+                //if (index == 3) name = "StopWork";
+            }
+            else if (nodeType == "scnStartNode")
+            {
+                if (index == 0) name = "Out";
+            }
+            else if (nodeType == "scnRewindableSectionNode")
+            {
+                if (index == 0) name = "CancelFwd";
+                if (index == 1) name = "TransmitSignal";
+            }
+            else if (nodeType == "scnCutControlNode")
+            {
+                if (index == 0) name = "Out";
+                if (index == 1) name = "CutSource";
+            }
+            else
+                if (index == 0) name = "Out";
+
+            return name != "" ? name + " " : "";
+        }
+
+        public static List<ItemInput> GetInputsNames(string nodeType, int inputVarCount = 1)
+        {
+            List<ItemInput> names = [];
+
+            if (nodeType == "scnChoiceNode")
+            {
+                names.Add(new() { InputName = "In", Name = "0", Ordinal = "0" });
+                names.Add(new() { InputName = "Cancel", Name = "1", Ordinal = "0" });
+                names.Add(new() { InputName = "ReactivateGroup", Name = "2", Ordinal = "0" });
+                names.Add(new() { InputName = "TimeLimitedFinish", Name = "3", Ordinal = "0" });
+            }
+            else if (nodeType == "scnEndNode")
+            {
+                names.Add(new() { InputName = "In", Name = "0", Ordinal = "0" });
+            }
+            else if (nodeType == "scnStartNode")
+            {
+            }
+            else if (nodeType == "scnRewindableSectionNode")
+            {
+                names.Add(new() { InputName = "In", Name = "0", Ordinal = "0" });
+                names.Add(new() { InputName = "Cancel", Name = "1", Ordinal = "0" });
+                names.Add(new() { InputName = "Pause", Name = "2", Ordinal = "0" });
+                names.Add(new() { InputName = "ForwardNormal", Name = "3", Ordinal = "0" });
+                names.Add(new() { InputName = "ForwardSlow", Name = "4", Ordinal = "0" });
+                names.Add(new() { InputName = "ForwardFast", Name = "5", Ordinal = "0" });
+                names.Add(new() { InputName = "BackwardNormal", Name = "6", Ordinal = "0" });
+                names.Add(new() { InputName = "BackwardSlow", Name = "7", Ordinal = "0" });
+                names.Add(new() { InputName = "BackwardFast", Name = "8", Ordinal = "0" });
+                names.Add(new() { InputName = "ForwardVeryFast", Name = "9", Ordinal = "0" });
+                names.Add(new() { InputName = "BackwardVeryFast", Name = "10", Ordinal = "0" });
+            }
+            else if (nodeType == "scnCutControlNode")
+            {
+                names.Add(new() { InputName = "In", Name = "0", Ordinal = "0" });
+            }
+            else if (nodeType == "scnAndNode")
+            {
+                for (int i = 0; i < inputVarCount; i++)
+                    names.Add(new() { InputName = "In", Name = "0", Ordinal = i.ToString() });
+                names.Add(new() { InputName = "Cancel", Name = "1", Ordinal = "0" });
+            }
+            else
+            {
+                names.Add(new() { InputName = "In", Name = "0", Ordinal = "0" });
+                names.Add(new() { InputName = "Cancel", Name = "1", Ordinal = "0" });
+            }
+
+            return names;
+        }
+
+        List<Color> linesColors = new();
 
 		private void AddColors()
 		{
